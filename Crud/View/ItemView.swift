20e4f1aba86e380cda535081c86a5efa91501396
Doni <donizetti.souza9@gmail.com>
2024-05-18 18:24:03 -0300

    - Renaming items

---
+++ Crud/View/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemUser.swift
+//  Crud
+//
+//  Created by Donizetti de Souza on 5/18/24.
+//
+
+import SwiftUI
+
+struct ItemUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemUser()
+}
