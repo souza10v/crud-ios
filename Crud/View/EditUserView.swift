9970f258e4af70d71b2b1ec50909e333ca271fc3
Doni <donizetti.souza9@gmail.com>
2024-05-23 22:20:58 -0300

    - addinng a X button to close when createUserView  and EditUserView are open using sheet mode

---
+++ Crud/View/EditUserView.swift
@@ -0,0 +1,18 @@
+//
+//  EditUserView.swift
+//  Crud
+//
+//  Created by Donizetti de Souza on 5/23/24.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUserView()
+}
