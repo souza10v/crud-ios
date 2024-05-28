0daeea8ba6f509a2d8e24d56f63433f61ba84411
Doni <donizetti.souza9@gmail.com>
2024-05-27 21:22:35 -0300

    -delete api created

---
+++ Crud/View/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Crud
+//
+//  Created by Donizetti de Souza on 5/27/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
